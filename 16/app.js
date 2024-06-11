a8d6b471d403bdf8d27624a046b3183d3803b671
mog <lbentbagdad@gmail.com>
2024-06-11 15:54:20 +0200

    API MEAL - 6

--- 16/app.js
+++ 16/app.js
@@ -19,11 +19,20 @@ rainbow();
 
 
 const form = document.getElementById("form");
+const msg = document.getElementById("msg");
 
 const search = (e) =>{
     e.preventDefault();
     let inputValue = document.getElementById("search").value;
-    console.log(inputValue);
+    if (!inputValue){
+        msg.innerText = "Invalid item";
+    } else {
+        msg.innerText = `These are the results for "${inputValue}"`;
+    }
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
 }
 
 form.addEventListener("submit", search);
